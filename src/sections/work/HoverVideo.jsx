9224c54ea3d67d83ccb682d38e250aca3c48f909
Susu-spec <suleimansuwaibat@gmail.com>
2025-10-09 20:15:12 +0100

    feat: adds project-blocks components to work section

---
+++ src/sections/work/HoverVideo.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+import { useCursor } from "../../context/CursorProvider";
+
+export default function HoverVideo({ src, poster }) {
+  const videoRef = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
+  const { setView, setDefault } = useCursor();
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    if (isHovered) {
+        video.currentTime = 0;
+        video.play().catch(() => {});
+    } else {
+        video.pause();
+        video.currentTime = 0;
+    }
+    }, [isHovered]);
+
+  return (
+    <div
+      onMouseEnter={() => {
+        setView()
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => {
+        setDefault();
+        setIsHovered(false)
+    }}
+    >
+      <video
+        ref={videoRef}
+        src={src}
+        poster={poster}
+        muted
+        loop
+        playsInline
+        autoPlay={isHovered}
+        style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            transition: "opacity .3s ease-in-out"
+        }}
+      />
+    </div>
+  );
+}
---
+++ src/sections/work/ProjectBlocks.jsx
@@ -0,0 +1,98 @@
+import { Box, Grid, Flex } from "@chakra-ui/react";
+import HoverVideo from "./HoverVideo";
+import { TbFlagExclamation } from "react-icons/tb";
+import { AnimatePresence, motion } from "framer-motion";
+import SplitText from "../../components/shared/SplitText";
+
+export default function ProjectBlocks() {
+    return (
+        <Grid
+            columnGap="1vw"
+            rowGap="1vw"
+            paddingTop="4vw"
+            paddingX="1vw"
+        >
+            <Grid
+                alignItems="stretch"
+                columnGap="1vw"
+                rowGap="1vw"
+                display="flex"
+            >
+                <div className="landscape home-video-container">
+                    <HoverVideo
+                        src="http://res.cloudinary.com/dm7vlpslq/video/upload/v1759642096/bheeicdi21w0hkxyqiby6vxovghp64qh2wan_qwmt7l.mp4"
+                        poster="https://res.cloudinary.com/dgafp4dx4/image/upload/v1760014047/oreo-thumbnail_ahdxpv.jpg"
+                    />
+                </div>
+                <div className="portrait home-video-container">
+                    <HoverVideo
+                        src="https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642676/YThAIwoXKB_576p_1744824072_ccmg6f.mp4"
+                        poster="https://res.cloudinary.com/dgafp4dx4/image/upload/v1760014077/icecream-thumbnail_zytvtb.jpg"
+                    />
+                </div>
+            </Grid>
+             <Grid
+                alignItems="stretch"
+                columnGap="1vw"
+                rowGap="1vw"
+                // alignItems="flex-start"
+                display="flex"
+            >
+                 <div className="portrait home-video-container">
+                    <Flex
+                        zIndex={5}
+                        justifyContent="space-between"
+                        alignItems="flex-start"
+                        paddingTop="1vw"
+                        paddingX="1vw"
+                        position="absolute"
+                    >
+                        <Flex
+                            alignItems="flex-start"
+                        >
+                            <Flex
+                                rowGap=".6vw"
+                                columnGap=".6vw"
+                                justifyContent="flex-start"
+                                alignItems="center"
+                                width="15vw"
+                            >
+                                <AnimatePresence>
+                                    <motion.div
+                                        initial={{ opacity: 1 }}
+                                        animate={{ opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                    >
+                                        <SplitText
+                                        initial={{ y: '100' }}
+                                        animate="visible"
+                                        variants={{
+                                            visible: i => ({
+                                                y: 0,
+                                                transition: {
+                                                    delay: i * 0.1
+                                                }}
+                                            )
+                                        }}>
+                                            Oreo Pacman
+                                        </SplitText>
+                                    </motion.div>
+                                </AnimatePresence>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                    <HoverVideo
+                        src="https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642080/bheeicea8cwvkf21vcnbg8jhx3wo0wyb42im_q9yvuz.mp4"
+                        poster="https://res.cloudinary.com/dgafp4dx4/image/upload/v1760014816/coin-thumbnail-new_o9njeo.jpg"
+                    />
+                </div>
+                <div className="landscape home-video-container">
+                    <HoverVideo
+                        src="https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642089/bheeicea3wowr2ifyqckn5737cnu42mewpr1_a8wqzo.mp4"
+                        poster="https://res.cloudinary.com/dgafp4dx4/image/upload/v1760014101/goggles_dzwjen.jpg"
+                    />
+                </div>
+            </Grid>
+        </Grid>
+    )
+}
