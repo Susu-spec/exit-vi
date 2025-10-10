d42d1ed1d76eeb296dd8ed4c8475ea337205e9cc
Susu-spec <suleimansuwaibat@gmail.com>
2025-10-10 17:35:28 +0100

    feat: adds brand-swiper component

--- src/lib/data.js
+++ src/lib/data.js
@@ -92,3 +92,66 @@ export const awards = [
         year: "2024"
     }
 ];
+
+export const brandsData = [
+    // Column 1
+    [
+        {
+            name: "Powerade",
+            logoDefault: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e34e47ff0775e1c2b8_logo_Powerade.svg",
+            logoHover: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e22a8a2708eb8d48c6_logo_Powerade--white.svg",
+            hoverClass: "_01"
+        },
+        {
+            name: "Spotify",
+            logoDefault: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67ddd9d69b96eb906bd561ef_logo_Spotify.svg",
+            logoHover: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67ddd9d6664033ef9d0a2c5a_logo_Spotify--white.svg",
+            hoverClass: "_05"
+        }
+    ],
+    // Column 2
+    [
+        {
+            name: "Doordash",
+            logoDefault: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e3e358457209bcadb8_logo_Doordash.svg",
+            logoHover: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e3a04d22ccb8a4055d_logo_Doordash--white.svg",
+            hoverClass: "_02"
+        },
+        {
+            name: "Oreo",
+            logoDefault: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e44d76c91cbd0a8b60_logo_Oreo.svg",
+            logoHover: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e38dcb3c792378fcf3_logo_Oreo-white.svg",
+            hoverClass: "_06"
+        }
+    ],
+    // Column 3
+    [
+        {
+            name: "Google",
+            logoDefault: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e26165aba082f8a5f2_logo_Google.svg",
+            logoHover: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e22e4501b9a0d48590_logo_Google--white.svg",
+            hoverClass: "_03"
+        },
+        {
+            name: "Ogilvy",
+            logoDefault: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e35934a71f11cce411_logo_Ogilvy.svg",
+            logoHover: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e3d3b7ed6b7d1e8d93_logo_Ogilvy--white.svg",
+            hoverClass: "_07"
+        }
+    ],
+    // Column 4
+    [
+        {
+            name: "Coca-Cola",
+            logoDefault: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e30170e1fc0f157f58_logo_Coca-Cola.svg",
+            logoHover: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e31064fa76ec051bcf_logo_Coca-Cola--white.svg",
+            hoverClass: "_04"
+        },
+        {
+            name: "Saatchi & Saatchi",
+            logoDefault: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e4d04280190712544d_logo_Saatchi-Saatchi.svg",
+            logoHover: "https://cdn.prod.website-files.com/6403ade6e2cfd9c42fe587b0/67db30e44a10d1c4f3125118_logo_Saatchi-Saatchi--white.svg",
+            hoverClass: "_08"
+        }
+    ]
+]
